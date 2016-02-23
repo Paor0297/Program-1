760e120ba23aebf21cce369ba7b709323d50f131
Paor0297 <pabloandre.ortizrojas@gmail.com>
2016-02-23 12:03:58 -0400

    Added files via upload

---
+++ HOLA.MUNDO.cpp
@@ -0,0 +1,19 @@
+// Este codigo ha sido generado por el modulo psexport 20140901-w32 de PSeInt
+// dado que dicho modulo se encuentra aun en desarrollo y en etapa experimental
+// puede que el codigo generado no sea completamente correcto. Si encuentra
+// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+#include<iostream>
+using namespace std;
+
+// Para leer variables de texto se utiliza el operador << del objeto cin, que
+// lee solo una palabra. Para leer una linea completa (es decir, incluyendo los
+// espacios en blanco) se debe utilzar getline (ej, reemplazar cin>>x por
+// getline(cin,x)), pero obliga a agregar un cin.ignore() si antes del getline
+// se leyó otra variable con >>.
+
+int main() {
+	cout << "HOLA MUNDO" << endl;
+	return 0;
+}
+
